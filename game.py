a438b9bb1cf3124fa2eff3328a6002c10ecfb5a3
OnlyVerga <lorenzo.vergani.270904@gmail.com>
2020-10-03 13:19:46 +0200

    commit

--- game.py
+++ game.py
@@ -7,9 +7,40 @@ WIN_DIM = (600, 400)
 window = pygame.display.set_mode(WIN_DIM)
 display = pygame.Surface((WIN_DIM[0] / 2, WIN_DIM[1] / 2))
 
+#       setting up map and other stuff
+map = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
 while True:
+    #       event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print("w")
+
+        #       blitting
+        for a in range(len(map)):
+            for b in range(len(map[0])):
+                if map[a][b] == 0:
+                    pygame.draw.rect(display, (255, 0, 0), (16 * b, 16 * a, 16, 16))
+                else:
+                    pygame.draw.rect(display, (0, 0, 255), (16 * b, 16 * a, 16, 16))
+
+    window.blit(pygame.transform.scale(display, WIN_DIM), [0, 0])
     pygame.display.update()
